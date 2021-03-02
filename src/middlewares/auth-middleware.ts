f785d52b2875cf5d62a7c12ae051ebfef7fdc753
Jakub Kus <kuskuba@gmail.com>
2021-03-02 10:56:11 +0000

    Added option to auth middleware to check for user role

--- src/middlewares/auth-middleware.ts
+++ src/middlewares/auth-middleware.ts
@@ -1,34 +1,46 @@
-import { RequestHandler } from "express";
-import { handle } from "../common/handler";
+import { RequestHandler } from 'express';
+import { handleBasic } from '../common/handler';
 import Jwt, { TokenExpiredError } from 'jsonwebtoken';
-import { SECRET_KEY } from "../common/config";
-import { Users } from "../models/users";
+import { SECRET_KEY } from '../common/config';
+import { Users } from '../models/users';
+import { Roles } from '../models/roles';
+import { BadRequestError } from '../common/errors/bad-request';
 
-export function authentication(): RequestHandler {
-    return handle(async ({ req, res, next}) => {
-        const token = req.headers['x-access-token'];
-        if (!token) {
-            throw new Error("Token not provided")
-        }
-        if (typeof token !== 'string') {
-            throw new Error("Token not set in the header");
-        }
+export interface AuthOptions {
+  roles?: Roles.Type[];
+}
 
-        try {
-        const decoded = Jwt.verify(token, SECRET_KEY, );
-            if (typeof decoded !== 'object') {
-                throw new Error("Failed to decode token");
-            }
-            const tokenObject = decoded as unknown as Users.Token
-            res.locals.id = tokenObject.id;
-            res.locals.role = tokenObject.role;
-        } catch(err) {
-            if (err instanceof TokenExpiredError) {
-                throw new Error("Token expired!")
-            } else {
-                throw new Error("Invalid token!")
-            }
+export function authenticate(options?: AuthOptions): RequestHandler {
+  return handleBasic(async ({ req, res, next }) => {
+    const token = req.headers['x-access-token'];
+    if (!token) {
+      throw new Error('Token not provided');
+    }
+    if (typeof token !== 'string') {
+      throw new Error('Token not set in the header');
+    }
+
+    try {
+      const decoded = Jwt.verify(token, SECRET_KEY);
+      if (typeof decoded !== 'object') {
+        throw new Error('Failed to decode token');
+      }
+      const tokenObject = (decoded as unknown) as Users.Token;
+      res.locals.id = tokenObject.id;
+      res.locals.role = tokenObject.role;
+
+      if (options && options.roles) {
+        if (!options.roles.includes(tokenObject.role)) {
+          throw new BadRequestError('You do not have permission to access this resource');
         }
-        next()
-    })
-} 
+      }
+    } catch (err) {
+      if (err instanceof TokenExpiredError) {
+        throw new Error('Token expired!');
+      } else {
+        throw new Error('Invalid token!');
+      }
+    }
+    next();
+  });
+}
